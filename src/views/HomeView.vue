b29b910f213cd95784e606ba303688a08fa79e8e
kiboma <omayioben@gmail.com>
2023-02-27 07:32:54 +0300

    Use setup method

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="home">
-
+    <H1>This is my home page</H1>
+    <p>My name is {{ name }} and my age is {{ age }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HomeView',
-  components: {}
+
+  setup(){
+    console.log("set up done ..")
+
+    let name = 'Kiboma'
+    let age = 31
+
+    return {name, age}
+  }
+
 }
 </script>
